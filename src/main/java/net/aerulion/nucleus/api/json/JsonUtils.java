13d0b96e952e1804767d10bd2471636bcc5a68e8
aerulion <aerulion@gmail.com>
2020-09-17 14:02:20 +0200

    1.16.3 Update

--- src/main/java/net/aerulion/nucleus/api/json/JsonUtils.java
+++ src/main/java/net/aerulion/nucleus/api/json/JsonUtils.java
@@ -1,12 +1,12 @@
 package net.aerulion.nucleus.api.json;
 
-import net.minecraft.server.v1_16_R1.NBTTagCompound;
-import org.bukkit.craftbukkit.v1_16_R1.inventory.CraftItemStack;
+import net.minecraft.server.v1_16_R2.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_16_R2.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
 public class JsonUtils {
     public static String convertItemStackToJson(ItemStack itemStack) {
-        net.minecraft.server.v1_16_R1.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
+        net.minecraft.server.v1_16_R2.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
         NBTTagCompound compound = new NBTTagCompound();
         nmsItemStack.save(compound);
         return compound.toString();
