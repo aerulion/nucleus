b299296a80c86fc39be2cf2025f43d1be93c6339
aerulion <aerulion@gmail.com>
2021-05-18 04:22:01 +0200

    Set gui handler to lowest priority

--- src/main/java/net/aerulion/nucleus/util/GuiHandler.java
+++ src/main/java/net/aerulion/nucleus/util/GuiHandler.java
@@ -2,6 +2,7 @@ package net.aerulion.nucleus.util;
 
 import net.aerulion.nucleus.api.gui.GUI;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -9,7 +10,7 @@ import org.bukkit.inventory.InventoryHolder;
 import org.jetbrains.annotations.NotNull;
 
 public class GuiHandler implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onGuiClick(@NotNull InventoryClickEvent event) {
         InventoryHolder inventoryHolder = event.getInventory().getHolder();
         if (inventoryHolder instanceof GUI) {
@@ -19,7 +20,7 @@ public class GuiHandler implements Listener {
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onGuiClose(@NotNull InventoryCloseEvent event) {
         InventoryHolder inventoryHolder = event.getInventory().getHolder();
         if (inventoryHolder instanceof GUI) {
